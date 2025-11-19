94c75be1b5825f4d87654e3acdad5f24242c40c7
diogogoncalves <dmgdark95@gmail.com>
2025-11-19 10:07:29 +0000

    Add API Auth

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
